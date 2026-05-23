02187658c482d4820965dd39146f9f2d31d800e7
xandt3ch <127065567+xandt3ch@users.noreply.github.com>
2026-05-22 20:39:26 -0400

    agregando comentario a las rutas

--- app/route.py
+++ app/route.py
@@ -4,30 +4,33 @@ from flask import  Blueprint, render_template, url_for
 main = Blueprint('main',__name__)
 auth = Blueprint('auth',__name__)
 
-
+#ruta de inicio
 @main.route('/')
 def dashboard():
     return render_template('index.html')
 
+# ruta de prestamo
 @main.route('/prestamo')
 def prestamo():
     return render_template('prestamo.html')
 
+# ruta plus
 @main.route('/plus')
 def plus():
     return render_template('plus.html')
 
+#ruta cliente
 @main.route('/cliente')
 def cliente():
     return render_template('cliente.html')
 
+# ruta de pago
 @main.route('/pago')
 def pago():
     return render_template('pago.html')
 
-
+# ruta de configuracion
 @main.route('/config')
 def config():
     return render_template('config.html')
-
 
